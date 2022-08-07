166235a3f55ef84eb4f2607f779e3ae489dda07f
Athar786 <atharerit@gmail.com>
2022-08-07 16:31:02 +0530

    remove requrest

--- app/Http/Controllers/ShortURLController.php
+++ app/Http/Controllers/ShortURLController.php
@@ -12,7 +12,7 @@ class ShortURLController extends Controller
         return view('welcome',compact('urls'));
     }
 
-    public function create(Request $request)
+    public function create()
     {
         $bulider = new \AshAllenDesign\ShortURL\Classes\Builder();
         $shortURLObj = $bulider->destinationUrl(request()->url)->make();
